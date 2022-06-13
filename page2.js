4f53d68075bbf8f28f68ebd1f5559a30166cd17f
Rogerio Kotsubo <rogeriokotsubo@gmail.com>
2022-06-13 11:44:45 -0300

    chore: using input event exercise 2

--- page2.js
+++ page2.js
@@ -1,7 +1,8 @@
 const cepInputObj = document.querySelector("#cep");
 let cep=cepInputObj.value;
-cepInputObj.addEventListener('keyup', validateInput);
+cepInputObj.addEventListener('keyup', limita8);
 cepInputObj.addEventListener('keydown', validateBackSpace);
+cepInputObj.addEventListener('input', validateInput);
 
 function validateBackSpace(e) {
   if (e.key==='Backspace'){
@@ -13,7 +14,37 @@ function validateBackSpace(e) {
 }
 
 function validateInput(e){
-  switch (e.key){
+  switch (e.data){
+    case '0':  // 0 
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':   //9
+      break;
+    default:
+      cepInputObj.value = cep;
+  }    
+}
+
+function limita8(e){
+  const newcep = cepInputObj.value.replace('-','');
+  const lenCEP = newcep.length;
+  if (lenCEP > 8){
+    cepInputObj.value = cep;
+  } else 
+  if (lenCEP>5) {
+    cepInputObj.value = newcep.substr(0,5)+'-'+newcep.substr(5);        
+  } 
+  cep=cepInputObj.value;
+}
+
+function validateInputSemNecessidadeKeyup(e){
+  switch (e.data){
     case '0':  // 0 
     case '1':
     case '2':
@@ -28,7 +59,8 @@ function validateInput(e){
       const lenCEP = newcep.length;
       if (lenCEP > 8){
         cepInputObj.value = cep;
-      } else if (lenCEP>5) {
+      } else 
+      if (lenCEP>5) {
         cepInputObj.value = newcep.substr(0,5)+'-'+newcep.substr(5);        
       } 
       cep = cepInputObj.value;
